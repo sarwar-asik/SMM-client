89d6b91a18d29eb0801129d2558f8d9155f7f0d3
sarwar-asik <shasik2021@gmail.com>
2023-03-02 13:37:52 +0600

    edited

--- src/pages/routes/AddBlogs.js
+++ src/pages/routes/AddBlogs.js
@@ -12,7 +12,7 @@ const AddBlogs = () => {
   const onSubmit = (data) => {
     console.log(data);
     Swal.fire("sent", "", "success");
-  };
+  }
 
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
--- src/pages/routes/EditBlogs.js
+++ src/pages/routes/EditBlogs.js
@@ -49,7 +49,7 @@ const EditBlogs = () => {
     },
   ];
   return (
-    <div>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <h2 className="text-4xl font-bold font-serif my-2  mx-auto w-[100%] text-center">
         {" "}
         Your Blogs{" "}
